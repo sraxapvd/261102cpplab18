297042a60b89cfcbff6a8cc8c98ac3f123621654
Rada <sirada_dee@cmu.ac.th>
2026-02-12 21:33:08 +0700

    lab18_1.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2){
+	double left   = max(r1.x, r2.x);
+    double right  = min(r1.x + r1.w, r2.x + r2.w);
 
+    double top    = min(r1.y, r2.y);
+    double bottom = max(r1.y - r1.h, r2.y - r2.h);
 
+    double width  = right - left;
+    double height = top - bottom;
 
+    if(width <= 0 || height <= 0)
+        return 0;
+
+    return width * height;
 }
